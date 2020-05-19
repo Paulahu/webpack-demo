dac2c75eaf3ae24677ad4d26cbc3c5af681ef55e
Paulahu <paulahu1995@gmail.com>
2020-05-19 14:12:06 +0800

    update

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
+module.exports = {
+    mode:'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'index.[contenthash].js'
+    },
+    plugins: [new HtmlWebpackPlugin(
+        {
+            title: 'webpack_demo',
+            template: 'src/assets/index.html'
+        }
+    )],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+}
